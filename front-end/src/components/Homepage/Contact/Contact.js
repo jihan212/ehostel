7fd26ef435511590edc98a072c9f72ee82fa4da1
snapper <zihanjashim2121@gmail.comm>
2022-03-07 22:53:15 +0600

    Updated

--- front-end/src/components/Homepage/Contact/Contact.js
+++ front-end/src/components/Homepage/Contact/Contact.js
@@ -1,9 +1,3 @@
-import {
-  faAddressCard,
-  faEnvelope,
-  faPhone,
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const Contact = () => {
